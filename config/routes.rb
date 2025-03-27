5fe714a2e9657a6749db1971bb14dcfe5ad9c751
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-03-27 21:24:07 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "pages", :action => "home"})
+
+  get("/square/new", {controller: "pages", action: ""})
+
+  get("/square/results", {controller: "pages", action: ""})
+
+  get("/square_root/new", {controller: "pages", action: ""})
+
+  get("/square_root/results", {controller: "pages", action: ""})
+
+  get("/payment/new", {controller: "pages", action: ""})
+
+  get ("/payment/results", {controller: "pages", action: ""})
+
+  get("/random/new", {controller: "pages", action: ""})
+
+  get("/random/results", {controller: "pages", action: ""})
   
 end
